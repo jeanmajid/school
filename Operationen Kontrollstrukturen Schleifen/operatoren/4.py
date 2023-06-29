d5fcecd307b2c679bc6d40f12d4e5480e80e023a
jeanmajid <jeanmajid@users.noreply.github.com>
2023-06-29 10:12:12 +0200

    updated 4

--- Operationen Kontrollstrukturen Schleifen/operatoren/4.py
+++ Operationen Kontrollstrukturen Schleifen/operatoren/4.py
@@ -1,4 +1,8 @@
 num1 = float(input("Erste zahl: "))
 num2 = float(input("Zweite zahl: "))
 
-print(f"Ergebnis: {num1 % num2}")
+def modulus(num1, num2):
+    return num1 - (num2 * (num1 // num2))
+
+
+print(modulus(num1, num2))
