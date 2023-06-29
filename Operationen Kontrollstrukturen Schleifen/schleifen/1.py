ba7bf45ff8d488fa9cc80461539a8ca2428396de
jeanmajid <jeanmajid@users.noreply.github.com>
2023-06-29 10:59:21 +0200

    omgomgomg

---
+++ Operationen Kontrollstrukturen Schleifen/schleifen/1.py
@@ -0,0 +1,11 @@
+currentPS = 50
+values = {}
+
+while currentPS <= 200:
+    values[currentPS] = currentPS * 1.35962
+    currentPS += 5
+print(values)
+
+Ps = float(input("PS: "))
+Kw = float(input("KW: "))
+
---
+++ Operationen Kontrollstrukturen Schleifen/schleifen/2.py
@@ -0,0 +1,4 @@
+num = int(input("Nummer: "))
+
+for n in range(1, num + 1):
+    print(n)
